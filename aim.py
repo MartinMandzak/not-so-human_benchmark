259f051101e4185470f395876c1d5f00b5c0a51a
MartinMandzak <102599044+MartinMandzak@users.noreply.github.com>
2023-10-24 23:11:40 +0200

    Trying to speed things up

--- aim.py
+++ aim.py
@@ -1,23 +1,27 @@
 import pyautogui
-from PIL import ImageGrab
 import numpy as np
+import mss
 import time
 
 screen_w,screen_h = pyautogui.size()
-region = (0,235,screen_w,screen_h-70)
-target_color = (149,195,232)
+target_color = (149,154,135)
+
 
 def find_and_click():
-    screenshot = ImageGrab.grab()
-    screenshot_np = np.array(screenshot)
-    coords = np.where(np.all(screenshot_np == target_color, axis=2))
-    x,y = coords[1][-1],coords[0][-1]
-    pyautogui.moveTo(x+15,y-15)
-    pyautogui.click()
+    with mss.mss() as peeper:
+        monitor = {"top": 235, "left" :0, "width": screen_w, "height": screen_h-50}
+        screenshot = peeper.grab(monitor)
+        screenshot = screenshot.rgb
+       #screenshot_np = np.array(screenshot)
+        coords = np.where(np.all(screenshot == target_color))
+        x,y = coords[1][-1],coords[1][-1]
+        pyautogui.moveTo(x+15,y-15)
+        pyautogui.click()
+        
 
 if __name__ == "__main__":
     time.sleep(2)
-    #pyautogui.moveTo(screen_w//2,screen_h//2+50)
+    #pyautogui.moveTo(screen_w//2,screen_h//2-55)
     #pyautogui.click()
     for click in range(32):
         find_and_click()
