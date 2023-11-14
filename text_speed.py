09482f93ed370abc5a02d5a4b956b0716c749c39
MartinMandzak <mandzak.martin3@gmail.com>
2023-11-14 10:51:40 +0100

    Typewriter completed

---
+++ text_speed.py
@@ -0,0 +1,19 @@
+"""
+Code that functions as a way to cheap the humanbenchmark's typewriter
+"""
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pyautogui
+import time
+
+browser = webdriver.Chrome()
+browser.get("https://humanbenchmark.com/tests/typing")
+data = browser.page_source
+
+soup = BeautifulSoup(data, 'html.parser')
+span_tags = soup.find_all('span', class_ = 'incomplete')
+
+time.sleep(5) #because accept cookies fucks me in the ass 
+pyautogui.typewrite(''.join([span.text for span in span_tags]))
+time.sleep(10)
